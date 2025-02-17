b89d8a85abf3f5034bf8644c6e0b4a17af67bde3
Eldoprano <snobistaintelectual@gmail.com>
2025-02-17 23:06:00 +0900

    quick commit.

--- repe/__init__.py
+++ repe/__init__.py
@@ -7,7 +7,9 @@ from .pipelines import repe_pipeline_registry
 # RepReading
 from .rep_readers import *
 from .rep_reading_pipeline import *
+from .pattern_reader import *
 
 # RepControl
 from .rep_control_pipeline import *
 from .rep_control_reading_vec import *
+from .pattern_control import *
